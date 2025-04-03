ad72307a724dea87713042c56ca0dcdbe185df0b
Kamil Witkowski <kamilwitkowskii@icloud.com>
2025-04-03 23:44:24 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ZSTspotted
 Source code of website "Spotted" of Zespołu Szkół Technicznych w Żninie.
+
+To install python packages required to run this web app:
+'''
+python3 -m venv venv
+source venv/bin/activate
+pip3 install mysql-connector-python
+pip3 install flask
+pip3 install flask_limiter
+python3 app.py
+'''
--- app.py
+++ app.py
@@ -1,29 +1,36 @@
 from flask import Flask, render_template, redirect, request, abort, make_response
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-import mysql.connector
 
 app = Flask(__name__)
-limiter = Limiter(app, key_func=get_remote_address)
 
-@app.before_request
+# Ustawienie Limiter bez błędów w przekazywaniu key_func
+limiter = Limiter(get_remote_address, app=app)
+'''@app.before_request
 def before_request():
-    if not request.is_secure and app.env != "development":
-        url = request.url.replace("http://", "https://", 1)
-        return redirect(url, code=301)
+    #if not request.is_secure and app.env != "development":
+        #url = request.url.replace("http://", "https://", 1)
+        #return redirect(url, code=301)
+    pass'''
+siteName = 'ZSTspotted'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', siteName=siteName)
 
+@app.route('/privacypolicy')
+def privacypolicy():
+    return render_template('legal/prywatnosc.html', siteName=siteName)
+
+# Apply limit only to POST requests
 @limiter.limit('1 per hour')
-@app.route('/sendanonymousmessage')
+@app.route('/sendanonymousmessage', methods=['POST'])
 def sendanonymousmessage():
     jsonData = request.get_json()
-    if not jsonData['message']:
+    if not jsonData or not jsonData.get('message'):  # Check if 'message' key exists
         abort(400)
     response = make_response('', 200)
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=400, debug=True)
