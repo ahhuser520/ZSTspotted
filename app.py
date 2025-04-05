1ce797ac8b270f6de4aab6af5d2e4df5c297f840
Kamil Witkowski <kamilwitkowskii@icloud.com>
2025-04-05 23:04:39 +0200

    mini update

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, redirect, request, abort, make_response, jsonify
-
+from flask import Flask, render_template, redirect, request, abort, make_response, jsonify, g
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import sqlite3 as sqlite
@@ -9,30 +8,31 @@ app = Flask(__name__)
 # Ustawienie Limiter bez błędów w przekazywaniu key_func
 limiter = Limiter(get_remote_address, app=app)
 
+siteName = 'ZSTspotted'
+
+# Przed każdym zapytaniem będziemy dodawać stopkę do kontekstu
 @app.before_request
-def before_request():
+def add_footer_to_context():
+    g.footer = render_template('modules/footer.html')  # Ładujemy footer.html do zmiennej globalnej
     '''if not request.is_secure:# and app.env != "development":
         url = request.url.replace("http://", "https://", 1)
         return redirect(url, code=301)'''
-    pass
-
-siteName = 'ZSTspotted'
 
 @app.route('/')
 def index():
-    return render_template('index.html', siteName=siteName)
+    return render_template('index.html', siteName=siteName, footer=g.footer)
 
 @app.route('/privacypolicy')
 def privacypolicy():
-    return render_template('legal/prywatnosc.html', siteName=siteName)
+    return render_template('legal/prywatnosc.html', siteName=siteName, footer=g.footer)
 
 @app.route('/post')
 def post():
-    return render_template('wyslij/index.html', siteName=siteName)
+    return render_template('wyslij/index.html', siteName=siteName, footer=g.footer)
 
 @app.route('/tos')
 def tos():
-    return render_template('legal/tos.html', siteName=siteName)
+    return render_template('legal/tos.html', siteName=siteName, footer=g.footer)
 
 # Apply limit only to POST requests
 @limiter.limit('1 per hour')
@@ -41,7 +41,7 @@ def sendanonymousmessage():
     jsonData = request.get_json()
     if not jsonData or not jsonData.get('message'):  # Check if 'message' key exists
         abort(400)
-    
+
     response = make_response('', 200)
     db = sqlite.connect('database.db')
     db.execute('''
@@ -60,17 +60,17 @@ def sendanonymousmessage():
         response.set_data(str(e))
     finally:
         db.close()
-    
+
     return response
 
 @app.route('/posty')
 def posty():
     page = request.args.get('page', default=1, type=int)
     postow_na_scroll = 50
-    
+
     # Wylicz offset wzgledem na obecna strone
     offset = (page - 1) * postow_na_scroll
-    
+
     db = sqlite.connect('database.db')
 
     posts = db.execute(
