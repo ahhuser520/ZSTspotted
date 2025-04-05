a9c836c6d541f7573dceabe75aa0ac72a47cfb66
windowscontrolpanel <wcphr@proton.me>
2025-04-05 19:40:44 +0000

    Dodano system postow

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, request, abort, make_response
+from flask import Flask, render_template, redirect, request, abort, make_response, jsonify
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+import sqlite3 as sqlite
 app = Flask(__name__)
 
 # Ustawienie Limiter bez błędów w przekazywaniu key_func
@@ -27,6 +28,8 @@ def post():
 def tos():
     return render_template('legal/tos.html', siteName=siteName)
 
+
+
 # Apply limit only to POST requests
 @limiter.limit('1 per hour')
 @app.route('/sendanonymousmessage', methods=['POST'])
@@ -35,7 +38,43 @@ def sendanonymousmessage():
     if not jsonData or not jsonData.get('message'):  # Check if 'message' key exists
         abort(400)
     response = make_response('', 200)
+    db = sqlite.connect('database.db')
+    db.execute('''
+        CREATE TABLE IF NOT EXISTS posty(
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+            content TEXT NOT NULL
+        )
+    ''')
+    try:
+        db.execute("INSERT INTO posty (content) VALUES (?)", (jsonData.get('message'),))
+        db.commit()
+        last_row_id = db.execute("SELECT last_insert_rowid()").fetchone()[0]
+        response.set_data(str(last_row_id))
+    except sqlite.Error as e:
+        response.status_code = 500
+        response.set_data(str(e))
+    finally:
+        db.close()
     return response
+
+@app.route('/posty')
+def posty():
+    page = request.args.get('page', default=1, type=int)
+    postow_na_scroll = 50
+    
+    # Wylicz offset wzgledem na obecna strone
+    offset = (page - 1) * postow_na_scroll
+    
+    db = sqlite.connect('database.db')
+
+    posts = db.execute(
+        "SELECT rowid, * FROM posty ORDER BY rowid DESC LIMIT ? OFFSET ?",
+        (postow_na_scroll, offset)
+    ).fetchall()
+
+    db.close()
+    return jsonify([{"id": post[0], "timestamp": post[1], "content": post[2]} for post in posts])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True)
